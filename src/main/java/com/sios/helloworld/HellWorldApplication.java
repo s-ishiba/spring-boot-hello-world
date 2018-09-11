7eb4408b062707bfac453f4cfcdba077e3045c1b
TAKEI Noriyuki <ntakei@sios.com>
2018-09-11 18:39:10 +0900

    first commit

---
+++ src/main/java/com/sios/helloworld/HellWorldApplication.java
@@ -0,0 +1,12 @@
+package com.sios.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellWorldApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sios/helloworld/HelloWorldContorller.java
@@ -0,0 +1,12 @@
+package com.sios.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldContorller {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World !!";
+	}
+}
