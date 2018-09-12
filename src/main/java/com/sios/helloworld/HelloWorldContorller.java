83bc0edbe86643392ab64d29b4bc89bceaf15922
TAKEI Noriyuki <ntakei@sios.com>
2018-09-12 20:36:50 +0900

    modified

--- src/main/java/com/sios/helloworld/HelloWorldContorller.java
+++ src/main/java/com/sios/helloworld/HelloWorldContorller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldContorller {
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello World !!hogehog4";
+		return "Hello World !!hogehog5";
 	}
 }
