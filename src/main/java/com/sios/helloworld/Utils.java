af8673db266b231af64c3ee86217019cb32b93c3
s-ishiba <s_ishiba@dreamarts.co.jp>
2021-02-08 15:39:04 +0900

    Update Utils.java

--- src/main/java/com/sios/helloworld/Utils.java
+++ src/main/java/com/sios/helloworld/Utils.java
@@ -3,7 +3,7 @@ package com.sios.helloworld;
 public class Utils {
 	
 	public static String hello() {
-		return "<h1 id='hello'>Hello World!!</h1>";
+		return "<h1 id='hello'>hello world</h1>";
 	}
 
 }
